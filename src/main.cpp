2f8dfce69a944b16e18a78f551ffe6c218c2e44a
Jan Wilczek <janekw@vp.pl>
2024-02-12 16:31:36 +0100

    Added try-catch

--- src/main.cpp
+++ src/main.cpp
@@ -6,36 +6,45 @@
 int main() {
 	namespace http = boost::beast::http;
 
-	boost::asio::io_context context;
-	boost::asio::ip::tcp::resolver resolver{context};
-	boost::beast::tcp_stream stream{context};
+    try {
+          boost::asio::io_context context;
+          boost::asio::ip::tcp::resolver resolver{context};
+          boost::beast::tcp_stream stream{context};
 
-	const auto host = "https://api.github.com";
-	const auto endpoint = "/repos/rust-lang-nursery/rust-cookbook/stargazers";
-	const auto results = resolver.resolve(host, "80");
+          const auto host = "https://api.github.com";
+          const auto endpoint =
+              "/repos/rust-lang-nursery/rust-cookbook/stargazers";
+          boost::asio::ip::tcp::resolver::query query{host, "http"};
+          const auto results = resolver.resolve(query);
 
-	stream.connect(results);
+          stream.connect(results);
 
-	http::request<http::string_body> request{http::verb::get, endpoint, 11};
-	request.set(http::field::host, host);
-	request.set(http::field::user_agent, BOOST_BEAST_VERSION_STRING);
+          http::request<http::string_body> request{http::verb::get, endpoint,
+                                                   11};
+          request.set(http::field::host, host);
+          request.set(http::field::user_agent, BOOST_BEAST_VERSION_STRING);
 
-	http::write(stream, request);
+          http::write(stream, request);
 
-	boost::beast::flat_buffer buffer;
+          boost::beast::flat_buffer buffer;
 
-	http::response<http::dynamic_body> result;
+          http::response<http::dynamic_body> result;
 
-	http::read(stream, buffer, result);
+          http::read(stream, buffer, result);
 
-	std::cout << result << std::endl;
+          std::cout << result << std::endl;
 
-	boost::beast::error_code ec;
-	stream.socket().shutdown(boost::asio::ip::tcp::socket::shutdown_both, ec);
+          boost::beast::error_code ec;
+          stream.socket().shutdown(boost::asio::ip::tcp::socket::shutdown_both,
+                                   ec);
 
-	if (ec && ec != boost::beast::errc::not_connected) {
-		throw boost::beast::system_error{ec};
-	}
+          if (ec && ec != boost::beast::errc::not_connected) {
+            throw boost::beast::system_error{ec};
+          }
+    } catch (const std::exception& e) {
+          std::cerr << "Error: " << e.what() << std::endl;
+          return EXIT_FAILURE;
+    }
 
 	return EXIT_SUCCESS;
 }
