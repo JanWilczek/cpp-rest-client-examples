cdcf563110ca95b57fa90e3a374ea0f4d16a56c2
Jan Wilczek <janekw@vp.pl>
2024-02-13 16:02:29 +0100

    Successful PUT request test.

--- src/cpp_httplib.cpp
+++ src/cpp_httplib.cpp
@@ -1,24 +1,35 @@
-#include <exception>
 #include <print>
 // Not needed probably thanks to the installation with vcpkg
 //#define CPPHTTPLIB_OPENSSL_SUPPORT
 #include <httplib.h>
 
 
-int main() {
-  // const auto host = "https://api.github.com";
-  const auto host = "jsonplaceholder.typicode.com";
-  httplib::SSLClient client(host, 443); 
-
-  const httplib::Headers headers {
-    { "User-Agent", "cpp-httplib" }
-  };
-  // const auto response = client.Get("/repos/rust-lang-nursery/rust-cookbook/stargazers", headers);
-  const auto response = client.Get("/posts", headers);
+void handleRequest(const auto& response) {
   if (response) {
     std::println("Status: {0}\nBody: {1}", response->status, response->body);
   } else {
     std::println("HTTP error: {}", httplib::to_string(response.error()));
   }
-
 }
+
+
+int main() {
+  // const auto host = "https://api.github.com";
+  const auto host = "jsonplaceholder.typicode.com";
+  httplib::SSLClient client{host}; 
+
+  std::println("====== GET request test ======");
+  const httplib::Headers headers {
+    { "User-Agent", "cpp-httplib" }
+  };
+  // const auto response = client.Get("/repos/rust-lang-nursery/rust-cookbook/stargazers", headers);
+  handleRequest(client.Get("/posts", headers));
+
+  std::println("====== PUT request test ======");
+  const httplib::Headers headers2 {
+    { "User-Agent", "cpp-httplib" },
+    { "Content-type", "application/json; charset=UTF-8"}
+  };
+  handleRequest(client.Put("/posts/1", headers, "{\n \"id\": 1,\n \"title\": \"foo\",\n \"body\": \"bar\",\n \"userId\": 1\n}", "application/json"));
+}
+
